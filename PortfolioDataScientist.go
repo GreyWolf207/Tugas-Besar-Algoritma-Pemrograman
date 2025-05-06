a1fc4a27ba9f0df044943a413085f6e6e2fa74a6
Irfaniahimsa <irfaniahimsa28@gmail.com>
2025-05-06 12:36:57 +0700

    Update PortfolioDataScientist.go

--- PortfolioDataScientist.go
+++ PortfolioDataScientist.go
@@ -2,174 +2,116 @@ package main
 
 import "fmt"
 
-const NMAX = 100
-type projects [NMAX]proyek
-type proyek struct {
+type projects [100]Project
+type Project struct {
 	nama string
 	kategori string
-	teknologi [10]string
-	jTeknologi int
-	kesulitan int
+	tingkatKesulitan int
 	waktu string
 }
 
 func main() {
-	var p projects
-	var n int
-	menu(&p, &n)
+	menu()
+	
 }
-
-func menu(projects *proyek, n *int) {
-	for {
-		fmt.Println("\n=== Aplikasi Manajemen Portofolio Data Science ===")
-		fmt.Println("1. Tambah Proyek")
-		fmt.Println("2. Tampilkan Semua Proyek")
-		fmt.Println("3. Ubah Proyek")
-		fmt.Println("4. Hapus Proyek")
-		fmt.Println("5. Cari Proyek (Sequential Search)")
-		fmt.Println("6. Cari Proyek (Binary Search)")
-		fmt.Println("7. Urutkan Proyek (Selection Sort)")
-		fmt.Println("8. Urutkan Proyek (Insertion Sort)")
-		fmt.Println("9. Statistik Proyek per Kategori")
-		fmt.Println("0. Keluar")
-		fmt.Print("Pilih menu: ")
-
-		var pilihan int
-		fmt.Scan(&pilihan)
-
-		switch pilihan {
-		case 1:
-			tambahProyek(projects, n)
-		case 2:
-			tampilkanProyek(*projects, *n)
-		case 3:
-			ubahProyek(projects, n)
-		case 4:
-			hapusProyek(projects, n)
-		case 5:
-			sequentialSearch(*projects, *n)
-		case 6:
-			binarySearch(projects, *n)
-		case 7:
-			selectionSort(projects, *n)
-			tampilkanProyek(*projects, *n)
-		case 8:
-			insertionSort(projects, *n)
-			tampilkanProyek(*projects, *n)
-		case 9:
-			statistikKategori(*projects, *n)
-		case 0:
-			fmt.Println("Keluar dari aplikasi.")
-		default:
-			fmt.Println("Pilihan tidak valid.")
+func menu(){
+	var data projects
+	var nProject int
+	for { 
+	fmt.Println("----------------------------------")
+	fmt.Println("|---Selamat Datang di Aplikasi---|")
+	fmt.Println("----------------------------------")
+	fmt.Println("1. Project User")
+	fmt.Println("2. Cari Project User")
+	fmt.Println("3. Urutkan Project User")
+	fmt.Println("4. Keluar Dari Aplikasi")
+	fmt.Print("pilih :")
+	var pilihan int
+	fmt.Scan(&pilihan)
+		switch pilihan {'
+			case 1:
+				ProjectUser(&data, &nProject)
+				var pilih int
+				fmt.Scan(&pilih)
+				switch pilih {
+					case 1:
+						tambahProject(&data, &nProject)
+					case 2:
+						tampilkanProject(data, nProject)
+					case 3:
+						ubahProject(&data, nProject)
+					//case 4:
+						//hapusProject()
+					}
+			//case 2:
+				//cariProject
+			//case 3:
+			//case 4:
+			default :
+				fmt.Println("Pilihan tidak valid!")
+			}
 		}
-	}
 }
 
-func tambahProyek(projects *proyek, n *int) {
-	var p Project
-	
-	if *n >= NMAX {
-		fmt.Println("Kapasitas proyek penuh.")
-	}
-
-	fmt.Print("Nama Proyek: ")
-	fmt.Scan(&p.nama)
-	fmt.Print("Kategori (contoh: machine_learning): ")
-	fmt.Scan(&p.kategori)
-	fmt.Print("Jumlah Teknologi (maks 10): ")
-	fmt.Scan(&p.jTeknologi)
-
-	if p.jTeknologi > 10 {
-		fmt.Println("Maksimum 10 teknologi!")
-		p.jTeknologi = 10
-	}
-
-	for i := 0; i < p.jTeknologi; i++ {
-		fmt.Printf("Teknologi ke-%d: ", i+1)
-		fmt.Scan(&p.teknologi[i])
-	}
-	
-	fmt.Print("Tingkat Kesulitan (1-5): ")
-	fmt.Scan(&p.kesulitan)
-	fmt.Print("Tanggal Pembuatan (dd-mm-yyy): ")
-	fmt.Scan(&p.waktu)
-
-	projects[*n] = p
-	*n++
-	fmt.Println("Proyek berhasil ditambahkan!")
+func ProjectUser(p *projects, nProject *int) {
+	fmt.Println("1. Tambah Project")
+	fmt.Println("2. Tampilkan Semua Project")
+	fmt.Println("3. Ubah Project")
+	fmt.Println("4. Hapus Project")
+	fmt.Print("Pilih :")
 }
 
-func tampilkanProyek(projects Project, n int) {
-	fmt.Println("\n=== Daftar Proyek ===")
-	
-	for i := 0; i < count; i++ {
-		p := projects[i]
-		fmt.Printf("[%d] %s | %s | %d | %s | Teknologi: ", i+1, p.Name, p.Category, p.Difficulty, p.Date)
+func tambahProject (p *projects, nProject *int) {
 		
-		for j := 0; j < p.TechCount; j++ {
-			fmt.Print(p.Technologies[j], " ")
-		}
+	fmt.Print("Nama Project: ")
+	fmt.Scan(&p[*nProject].nama)
+	fmt.Print("Tingkat kesulitan (1-10): ")
+	fmt.Scan(&p[*nProject].tingkatKesulitan)
+	fmt.Print("kategori: ")
+	fmt.Scan(&p[*nProject].kategori)
+	fmt.Print("Waktu (dd-mm-yyyy): ")
+	fmt.Scan(&p[*nProject].waktu)
+	
+	*nProject = *nProject + 1
+	fmt.Println("Project berhasil ditambahkan!!!")
+	fmt.Println()
+}
+func tampilkanProject (p projects, nProject int) {
+	if nProject == 0 {
+		fmt.Println("Maaf Data Belum Tersedia")
+	}
+	
+	
+	for i := 0; i < nProject; i++ {
+		fmt.Println("[Project]", i+1)
 		fmt.Println()
-	}
+		fmt.Println("Nama Project :", p[i].nama)
+		fmt.Println("kategori", p[i].kategori)
+		fmt.Println("Tingkat kesulitan (1-10):", p[i].tingkatKesulitan )
+		fmt.Println("Waktu (dd-mm-yyyy)", p[i].waktu)
+		fmt.Println()
+	} 
 }
-
-func ubahProyek(projects *Project, n *int) {
-	var idx int
-	
-	tampilkanProyek(*projects, *n)
-	
-	fmt.Print("Pilih nomor proyek yang ingin diubah: ")
-	fmt.Scan(&idx)
-	
-	if idx < 1 || idx > *n {
-		fmt.Println("Indeks tidak valid.")
-	}
-	
-	for i := idx - 1; i < *n-1; i++ {
-		projects[i] = projects[i+1]
-	}
-	*n--
-	
-	tambahProyek(projects, count)
-}
-
-func hapusProyek() {
-	var idx int
-	
-	tampilkanProyek(*projects, *count)
-	
-	fmt.Print("Pilih nomor proyek yang ingin dihapus: ")
-	fmt.Scan(&idx)
-	
-	if idx < 1 || idx > *count {
-		fmt.Println("Indeks tidak valid.")
-	}
-	
-	for i := idx - 1; i < *count-1; i++ {
-		projects[i] = projects[i+1]
-	}
-	
-	*count--
-	fmt.Println("Proyek berhasil dihapus!")
-} 
-
-func sequentialSearch() {
-	
-}
-
-func binarySearch() {
-	
-}
-
-func selectionSort() {
-	
-}
-
-func insertionSort() {
-	
-}
-
-func statistikKategori() {
-	
+func ubahProject(p *projects, nProject int) {
+	tampilkanProject(*p, nProject)
+		fmt.Print("Pilih nomor yang ingin diubah :")
+	var ubah int
+		fmt.Scan(&ubah)
+		ubah--
+		if ubah >= 0 && ubah < nProject {
+			fmt.Print("Nama Project baru : ")
+			fmt.Scan(&p[ubah].nama)
+			fmt.Print("Tingkat kesulitan (1-10): ")
+			fmt.Scan(&p[ubah].tingkatKesulitan)
+			fmt.Print("kategori: ")
+			fmt.Scan(&p[ubah].kategori)
+			fmt.Print("Waktu (dd-mm-yyyy): ")
+			fmt.Scan(&p[ubah].waktu)
+			fmt.Println()
+			fmt.Println("Project berhasil diubah!!")
+		} else {
+			fmt.Println()
+			fmt.Println("Nomor tidak sesuai!!")
+			fmt.Println()
+		}
 }
